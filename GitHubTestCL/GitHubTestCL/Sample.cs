cfed2a631f43d5735c68f26c2e65a67e3ef22cea
Rajyalakshmi Bathini <rajyalakshmi.bathini@tcs.com>
2016-06-20 09:36:42 -0400

    commit changes

---
+++ GitHubTestCL/GitHubTestCL/Sample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestCL
+{
+    public class Sample
+    {
+        public object Add(Int16 a , Int16 b)
+        {
+            return a + b;
+        }   
+         public void print()
+        {
+            Console.WriteLine("Welcome Page");
+        }
+    }
+}
